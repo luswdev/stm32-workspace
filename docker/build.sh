68072cec848021c20095113f1ecd456544a852c1
LuSkywalker <callumlu@gmail.com>
2023-04-06 23:31:27 +0800

    [M] build docker image in repo root

--- docker/build.sh
+++ docker/build.sh
@@ -5,10 +5,10 @@ ARCH=$(dpkg --print-architecture)
 case $ARCH in
     darwin-arm64)
         echo "Build docker image for Apple Silicon"
-        docker build --platform linux/arm64/v8 --network=host -t stm32-workspace .. -f toolchain.dockerfile
+        docker build --platform linux/arm64/v8 --network=host -t stm32-workspace . -f docker/toolchain.dockerfile
         ;;
     *)
         echo "Build docker image for general platform"
-        docker build --network=host -t stm32-workspace .. -f toolchain.dockerfile
+        docker build --network=host -t stm32-workspace . -f docker/toolchain.dockerfile
         ;;
 esac
