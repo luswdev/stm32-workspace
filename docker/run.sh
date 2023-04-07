fedfaa960b6cf8158514ff0c644ec08afa394a28
luswdev <610721230@gms.ndhu.edu.tw>
2023-04-07 13:37:29 +0800

    [BF] do not set tty mode if current is not terminal

--- docker/run.sh
+++ docker/run.sh
@@ -2,8 +2,17 @@
 
 RUN_CMD="$*"
 
-docker run -v $(pwd):/opt/project \
-    -it --rm  \
+if [ -t 1 ];then
+    docker run -v $(pwd):/opt/project \
+        -it --rm  \
+        --hostname stm32-workspace \
+        --workdir /opt/project \
+        stm32-workspace:latest $RUN_CMD
+else
+    docker run -v $(pwd):/opt/project \
+    -i --rm  \
     --hostname stm32-workspace \
     --workdir /opt/project \
     stm32-workspace:latest $RUN_CMD
+fi
+
