ebbd902e8a7c12178058d59e76ae087804286238
luswdev <610721230@gms.ndhu.edu.tw>
2023-04-07 12:56:45 +0800

    [M] run docker script with arguments support

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+RUN_CMD="$*"
+
 docker run -v $(pwd):/opt/project \
     -it --rm  \
     --hostname stm32-workspace \
     --workdir /opt/project \
-    stm32-workspace:latest zsh
+    stm32-workspace:latest $RUN_CMD
