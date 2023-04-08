2227a4c2cbe25ac14b727a79e72343c4234a18b6
LuSkywalker <callumlu@gmail.com>
2023-04-09 04:20:41 +0800

    [M] docker build script support custom arguments

--- docker/build.sh
+++ docker/build.sh
@@ -2,13 +2,15 @@
 
 ARCH=$(dpkg --print-architecture)
 
+RUN_CMD="$*"
+
 case $ARCH in
     darwin-arm64)
         echo "Build docker image for Apple Silicon"
-        docker build --platform linux/arm64/v8 --network=host -t stm32-workspace . -f docker/toolchain.dockerfile
+        docker build --platform linux/arm64/v8 --network=host -t stm32-workspace . -f docker/toolchain.dockerfile $RUN_CMD
         ;;
     *)
         echo "Build docker image for general platform"
-        docker build --network=host -t stm32-workspace . -f docker/toolchain.dockerfile
+        docker build --network=host -t stm32-workspace . -f docker/toolchain.dockerfile $RUN_CMD
         ;;
 esac
